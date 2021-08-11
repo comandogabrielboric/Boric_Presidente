7000c088e7823181bd784a7d6e6a73d9226f2e77
Monalmo <cristian.hadad.g@gmail.com>
2021-08-11 17:57:28 -0400

    base front con servidor node

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    indent: ['error', 'tab', { SwitchCase: 1 }],
+    // 'quote-props': ["error", "as-needed"],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-tabs': 'off',
+    curly: ['error', 'multi-line'],
+    'no-console': 'off',
+    'no-undef': ['warn', { typeof: false }],
+    // 'standard/computed-property-even-spacing': 'off',
+    'import/no-webpack-loader-syntax': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'no-throw-literal': 'off',
+    'no-multiple-empty-lines': 'off'
+  }
+}
