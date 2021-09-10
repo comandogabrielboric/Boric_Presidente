e991450c00bf5c0a170da5e6962d6a4949881c43
Monalmo <cristian.hadad.g@gmail.com>
2021-09-10 17:50:10 -0300

    footer

---
+++ components/Footer.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="contenedor-footer">
+    <div class="footer">
+      <div class="link-internos contenedor">
+        <nuxt-link class="link-footer" to="/storytelling">
+          <p>storytelling</p>
+        </nuxt-link>
+        <nuxt-link class="link-footer" to="/sumate">
+          <p>sumate</p>
+        </nuxt-link>
+        <nuxt-link class="link-footer" to="/dona">
+          <p>dona</p>
+        </nuxt-link>
+      </div>
+      <div class="link-rrss contenedor">
+        <div v-for="red in rrss" :key="red._id" class="link-rrss contenedor">
+          <a class="link-footer" :href="red.url"><img :src="red.icono.url" alt="" class="logorrss"></a>
+        </div>
+      </div>
+
+      <div class="part-ciudadana contenedor">
+        <nuxt-link class="link-footer" to="#">
+          participacion ciudadana
+        </nuxt-link>
+      </div>
+    </div>
+    <div v-for="logo in logosAD" :key="logo._id" class="logos contenedor-logos">
+      <img :src="logo.formats.thumbnail.url" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			rrss: [],
+			logosAD: null
+		}
+	},
+	async fetch () {
+		console.log('cargar footer')
+		const solicitud = await fetch(`${process.env.apiURL}/footer`).then(res =>
+			res.json()
+		)
+		this.logosAD = solicitud.logosAD
+		this.rrss = solicitud.RRSS
+		// console.log('footer: ', solicitud)
+		// console.log('logooooooos: ', this.logosAD)
+		console.log('rrss: ', this.rrss)
+	}
+}
+</script>
+
+<style scoped>
+.contenedor-footer {
+	border: 2px solid #fff;
+}
+.footer {
+	display: flex;
+	flex-flow: row;
+	width: 100vw;
+
+	justify-content: space-around;
+	align-items: center;
+}
+.link-footer {
+	color: #fff;
+}
+.contenedor {
+
+}
+.logos {
+	display: flex;
+	flex-flow: row;
+	color: #fff;
+	justify-content: center;
+	align-items: center;
+	padding-bottom: 10px;
+}
+.logorrss {
+	color: #fff;
+	width: 30px;
+}
+.link-rrss {
+	padding: 10px;
+	display: flex;
+	flex-flow: row;
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
 <style>
 body {
+	min-height: 100vh;
 	background-image: url('https://s3.amazonaws.com/cdn.boricpresidente.cl/archivos/5e5dd1be_body_1large_10000000nu19o0eh000028_7b219799f1.png');
 }
 </style>
