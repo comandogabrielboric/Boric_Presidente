3ab76a106dffcbcacb0fab11336c8de2620255e9
Monalmo <cristian.hadad.g@gmail.com>
2021-09-20 00:39:05 -0300

    suscribirse

--- components/participaant.vue
+++ components/participaant.vue
@@ -9,9 +9,12 @@ div
 			a-input(v-model='ruleForm.telefono' type='tel' placeholder='+56 x xxxx xxxx').input
 		a-form-model-item(has-feedback='' prop='comuna')
 			a-input(v-model='ruleForm.comuna' type='comuna' placeholder='Comuna').input
-		a-form-model-item(:wrapper-col='{ span: 14, offset: 4 }')
-			a-button(type='primary' @click="suscribirse('ruleForm')")
+		a-form-model-item(:wrapper-col='{ span: 14, offset: 4 }').contenedorbtn
+			a-button(type='primary' @click="suscribirse('ruleForm')").suscribirme
 				| SEGUIMOS
+		.terminosycondiciones
+			.acepto Acepto
+			.terminos Terminos y Condiciones
 
 </template>
 
@@ -70,7 +73,8 @@ export default {
 			layout: {
 				labelCol: { span: 4 },
 				wrapperCol: { span: 14 }
-			}
+			},
+			suscrito: null
 		}
 	},
 	methods: {
@@ -78,18 +82,46 @@ export default {
 			// const { nombre, email, telefono, comuna } = this
 			// const data = { nombre, email, telefono, comuna }
 			const config = {}
-			const respuesta = await this.$axios.post(`${process.env.apiURL}/suscribirse`, this.ruleForm, config).then(r => r.json).catch(e => console.error('fallo suscribirse', e))
+			const respuesta = await this.$axios.post(`${process.env.apiURL}/suscribirse`, this.ruleForm, config).then(r => r.data).catch(e => console.error('fallo suscribirse', e))
 			console.log('Respuesta', respuesta)
+			if (!respuesta) {
+				this.suscrito = false
+			}
+			console.log('suscrito', this.suscrito)
 		}
 	}
 }
 </script>
 
 <style lang="sass" scoped>
+@import '~/estilos/paleta'
 .suscribirse
 	display: flex
-	flex-flow: column
+	flex-wrap: wrap
+	justify-content: center
+	max-width: 400px
+
 	input
 		width: 250px
 		margin-top: 2px
+		border-radius: 2px
+		&::placeholder
+			font-size: .9em
+.suscribirme
+	// position: stycky
+	width: 250px
+	padding: 0
+	right: 42px
+	color: $petroleo1
+	background-color: $verde3
+	border-radius: 3px
+	font-weight: 900
+	font-size: 1.4em
+.terminosycondiciones
+	color: $verde3
+	display: flex
+	flex-flow: row
+	.terminos
+		font-weight: 900
+		margin-left: .4em
 </style>
