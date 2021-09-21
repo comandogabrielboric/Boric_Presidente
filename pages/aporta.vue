034601c87c2538b3d377f9e9964dfd8e9238ab78
Monalmo <cristian.hadad.g@gmail.com>
2021-09-21 00:24:48 -0300

    fin de hoy

--- pages/aporta.vue
+++ pages/aporta.vue
@@ -1,8 +1,44 @@
 <template lang="pug">
 .root
-	h1 Donar
+	.pasos
+		img(src="https://s3.amazonaws.com/cdn.boricpresidente.cl/archivos/small_1_117a57e95e.png")
+		img(src="https://s3.amazonaws.com/cdn.boricpresidente.cl/archivos/small_2_1835803185.png")
+		img(src="https://s3.amazonaws.com/cdn.boricpresidente.cl/archivos/small_3_82f194a838.png")
+		img(src="https://s3.amazonaws.com/cdn.boricpresidente.cl/archivos/small_4_4bac3fec41.png")
+
+		a(href="https://aportes.servel.cl/servel-aportes/inicio.xhtml" target="_blank") QUIERO APORTAR
+	.relleno
 </template>
 <script>
 export default {}
 </script>
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+@import '~/estilos/paleta'
+.root
+	.pasos
+		display: flex
+		flex-flow: column
+		align-items: center
+		background-color: $petroleo1
+		img
+			height: 700px
+			max-height: 100%
+		a
+			pading-bottom: -20em
+			display: flex
+			justify-content: center
+			border-radius: 2px
+			align-items: center
+			width: 320px
+			max-width: 70%
+			height: 2em
+			font-weight: 900
+			color: $azul2
+			background-color: $verde3
+.relleno
+	display: flex
+	width: 100vw
+	height: 200px
+	background-color: $petroleo1
+
+</style>
