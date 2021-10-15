d25a339d8a2e3e9b022f1aba433cbe989f127513
Monalmo <cristian.hadad.g@gmail.com>
2021-10-15 00:53:20 -0300

    crea participa

---
+++ pages/participa.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.rootParticipa
+	.seccion.header
+		img.imgHeader(src='/imagenes/homeParticipaMovil.webp')
+		h1.titulo Construyamos juntos el nuevo Chile
+	.seccion.mesasYConsulta
+	.seccion.uneteAUnaMesa
+	.seccion.contacto
+</template>
+<script>
+export default {}
+</script>
+<style lang="sass" scoped>
+.seccion
+	min-height: 400px
+.header
+	width: 100vw
+	.imgHeader
+		width: 100%
+	.titulo
+		color: #fff
+		display: flex
+		font-weight: 700
+		position: sticky
+
+</style>
