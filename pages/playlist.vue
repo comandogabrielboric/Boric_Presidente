4eec5c8b9d98a691a28ba74603f006fb80a3e534
Monalmo <cristian.hadad.g@gmail.com>
2021-10-28 11:08:11 -0300

    link playlist

--- pages/playlist.vue
+++ pages/playlist.vue
@@ -14,7 +14,7 @@
 			.titulo #[span.primero El Árbol]
 				div Ideas y Maquetas para un Chile nuevo
 			.listaSpotify
-				<iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DWZypDDXfbyfI?utm_source=generator" width="100%" height="100%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+				<iframe src="https://open.spotify.com/embed/playlist/4uKpFyC9PIeafFy80RAWS1?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
 			.subTitulo #[span.primero ¡Participa con una]
 				div pieza musical!
 			.texto
