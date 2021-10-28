ccc6c24f632b98ac8f1549b57dcee5e099582308
Monalmo <cristian.hadad.g@gmail.com>
2021-10-27 21:19:54 -0300

    p

---
+++ pages/programa.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.root
+	<a href="https://online.flippingbook.com/view/286470273/" class="fbo-embed" data-fbo-id="286470273" data-fbo-ratio="1:1" data-fbo-lightbox="yes" data-fbo-width="100%" data-fbo-height="auto" data-fbo-version="1" style="max-width: 100%">28_PROTECCIÓN Y RECUPERACIÓN DE LA BIODIVERSIDAD Y EL BIENE…</a><script async defer src="https://online.flippingbook.com/EmbedScriptUrl.aspx?m=redir&hid=286470273"></script>
+
+
+</template>
+<script>
+export default {}
+</script>
+<style lang="sass" scoped></style>
