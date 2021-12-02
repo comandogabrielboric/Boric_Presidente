bec524c30ff2d9821afde9688cd9ab48c71d7e96
Monalmo <cristian.hadad.g@gmail.com>
2021-12-02 18:29:54 -0300

    fix seo

--- pages/alcaldias.vue
+++ pages/alcaldias.vue
@@ -51,7 +51,7 @@ export default {
 	// solicita info a cms
 	head () {
 		// if (!this.seo) return {}
-		const titulo = this._.get(this.seo, ['titulo_pag'], 'Municipios x Boric')
+		const titulo = this._.get(this.seo, ['titulo_pag'], 'Alcaldias x Boric')
 		const descripcion = this._.get(
 			this.seo,
 			['descripcion_pag'],
