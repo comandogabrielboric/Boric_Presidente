7b95ab5b2c8d1e5cc73861c4ab9f7a9ce4ce0540
Monalmo <cristian.hadad.g@gmail.com>
2021-12-08 09:04:17 -0300

    nuevo pdf

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -27,7 +27,10 @@
 					to="/propuestas",
 					@click.native="tag('nav propuestas')"
 				) Propuestas
-				nuxt-link.link.bold(to="/alcaldias", @click.native="tag('nav municipios')") alcaldías
+				nuxt-link.link.bold(
+					to="/implementacion",
+					@click.native="tag('nav implementación')"
+				) implementación
 				//- nuxt-link.link.bold(to="/programa", @click.native="tag('nav programa')") Programa
 
 				//- nuxt-link.link.bold(to="/playlist", @click.native="tag('nav playlist')") playlist
